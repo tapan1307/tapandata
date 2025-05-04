a1e0f010b848e29615e4f6fc876c3ef2c1f468dd
tapan1307 <tapandey.1307@gmail.com>
2025-05-04 10:17:29 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+#Task 1:
+print('Task 1')
+print('Hello world')
+a=float(input('Enter First number- '))
+b=float(input('Enter Second number- '))
+print('Addition=',a+b)
+print('Subtraction=',a-b)
+print('Multiplication=',a*b)
+print('Division=',a/b)
+
+
+#Task 2:
+print('Task 2)')
+name1=input('Enter your First name: ')
+name2=input('Enter your Last name: ')
+print('Hello, ' + name1 +' ' + name2 + ' Welcome to the world of Python')
+print('Thank you')
---
+++ Assignment 2.py
@@ -0,0 +1,16 @@
+# Task 1:
+print("Task 1")
+a=int(input('Enter a number: '))
+if (a/2==int(a/2)):
+    print(a,',This is an Even number')
+else:
+    print(a,',This is an Odd number')
+
+
+# Task 2:
+print("Task 2")
+n=list(range(1,51))
+for i in n:
+    print(i)
+print('The sum of Numbers from 1 to 50 is: ', sum(range(1,51)))
+print('Thank You')
