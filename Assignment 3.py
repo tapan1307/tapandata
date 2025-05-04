74236427985262ca5c37e73bf24b0326e52f6aa8
tapan1307 <tapandey.1307@gmail.com>
2025-05-04 21:21:40 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,20 @@
+# Task 1:
+from math import radians
+
+
+def factorial(n):
+    if(n<=1):
+        return 1
+    else:
+        return n*factorial(n-1)
+
+result=factorial(5)
+print('Factorial of ',5,' is',result )
+
+# Task 2;
+
+import math
+x=int(input('Enter a number: '))
+print("Square root is", math.sqrt(x))
+print("Logarithm is", math.log(x))
+print("Sine is ", math.sin(x))
